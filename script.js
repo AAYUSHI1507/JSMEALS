f92df099b1b1b3093fc123d00ea440db592d133a
AAYUSHI1507 <a.kataria@somaiya.edu>
2022-07-12 18:04:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$.ajax({
+  type: "get",
+  url: "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian",
+  //   data: "data",
+  //   dataType: "dataType",
+  success: function (response) {
+    for (var i = 0; i < response.meals.length; i++) {
+      var newitem = `<div class='col-md-3 m-2 shadow-lg p-3 mb-5 bg-white rounded text-center'>
+        <p>${response.meals[i].strMeal}</p>
+        <img src = ${response.meals[i].strMealThumb} class = 'img-fluid' />
+        <p>${response.meals[i].idMeal}</p>
+        </div>`;
+
+      $("#myitems").append(newitem);
+    }
+  },
+  error: function (error) {
+    console.log(error);
+  },
+});
